62201e68c6cee45a8bc43f2f801300ead742455b
adarsh anchan <anchanadarsh@gmail.com>
2018-03-05 14:39:23 +0530

    flex-banner

---
+++ flex-banners/index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en-US">
+
+<head>
+    <title>Webpage Title</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <?php include '_partial/head.php' ?>
+</head>
+
+<body>
+    <div class="clearfix"></div>
+    <section id="section-banner" class="container-fluid p-lr-none bg-f1f">
+        <div class="int-banner banner-active "></div>
+        <div class="int-banner bg-yellow"></div>
+        <div class="int-banner bg-red"></div>
+        <div class="int-banner bg-blue"></div>
+        <div class="int-banner bg-green"></div>
+        <div class="int-banner bg-purple"></div>
+        <div class="int-banner bg-pink"></div>
+    </section>
+    <div class="clearfix"></div>
+    <?php include '_partial/footer-scripts.php' ?>
+</body>
+
+</html>
---
+++ flex-banners/js/main.js
@@ -0,0 +1,8 @@
+// All custom codes with comment
+
+$(document).ready(function () {
+    $(".int-banner").click(function () {
+        $(".int-banner").removeClass("banner-active");
+        $(this).addClass("banner-active");
+    });
+});
