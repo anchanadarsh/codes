e0ece8f9411613b7c0f6d77a6b95d9f873d00ec7
Adarsh Anchan <anchanadarsh@gmail.com>
2018-03-22 17:08:29 +0530

    sadasd

--- text-animation-onscroll/js/main.js
+++ text-animation-onscroll/js/main.js
@@ -1,26 +1,33 @@
 // All custom codes with comment
 
 $(document).ready(function () {
-
-    //    var itemH = $('.scroll-area h1').outerHeight();
-    //    var itemCount = $('.scroll-area h1').length;
-    //    $('.scroll-area').height(itemH * itemCount);
+    var winHeight = $(window).height();
+    var itemH = $('.scroll-area h1').outerHeight();
+    var itemHperc = (itemH / winHeight) * 100;
+    $('.toRight').each(function () {
+        var indR = $(this).index();
+        console.log(indR);
+        var assindR = 100 + ((indR / 2) * itemHperc);
+        console.log(assindR);
+        $(this).css('right', assindR + '%');
+    });
 });
 
 $(window).scroll(function () {
-
-    var winScroll = $(window).scrollTop();
     var winHeight = $(window).height();
-    var areaT = $('.scroll-area').offset().top;
+    var winScroll = $(window).scrollTop();
+    var areaT = $('.scroll-area h1').offset().top;
 
     var startAnim = areaT - winHeight;
     console.log(areaT);
     console.log(winHeight);
-    console.log("diff : " +startAnim);
-    console.log("scroll : " +winScroll);
+    console.log("diff : " + startAnim);
+    console.log("scroll : " + winScroll);
 
     if (winScroll > startAnim) {
-        $('.scroll-area .toLeft').css('left', '50%');
-        $('.scroll-area .toRight').css('right', '50%');
+        var posElem = 100 - (((winScroll - startAnim) / winHeight) * 100);
+        console.log("pos : " + posElem);
+        $('.scroll-area .toLeft').css('left', posElem + '%');
+        $('.scroll-area .toRight').css('right', posElem + '%');
     }
 });
