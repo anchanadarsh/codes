4945d6626167305406f2aa4ba9f2827017ca91be
Adarsh Anchan <anchanadarsh@gmail.com>
2018-05-14 19:07:26 +0530

    before after

---
+++ before-after/index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en-US">
+
+<head>
+    <title>Webpage Title</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <?php include '_partial/head.php' ?>
+</head>
+
+<body>
+    <div class="clearfix"></div>
+    <div class="creativity-section">
+        <button class="btn move_button">Move</button>
+        <section id="creativity-box"></section>
+        <section id="creativity-box-1"></section>
+    </div>
+    <?php include '_partial/footer-scripts.php' ?>
+</body>
+
+</html>
